db034fc0975795ebb4177b74cd44bf60af66066b
Kyle Barton <kyle.humberto@gmail.com>
2018-08-12 08:18:40 +0200

    got climbinb working

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -13,9 +13,12 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+    [SerializeField] private Transform m_ClimbCheck;
 
-	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+	const float k_GroundedRadius = .5f; // Radius of the overlap circle to determine if grounded
+    const float k_ClimbRadius = .5f;
 	private bool m_Grounded;            // Whether or not the player is grounded.
+    private bool m_Climbable;
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
@@ -25,12 +28,17 @@ public class CharacterController2D : MonoBehaviour
 	[Space]
 
 	public UnityEvent OnLandEvent;
+    public UnityEvent OnClimbEvent;
+    public UnityEvent OffGroundEvent;
 
 	[System.Serializable]
 	public class BoolEvent : UnityEvent<bool> { }
 
 	public BoolEvent OnCrouchEvent;
 	private bool m_wasCrouching = false;
+
+
+
 
 	private void Awake()
 	{
@@ -41,13 +49,18 @@ public class CharacterController2D : MonoBehaviour
 
 		if (OnCrouchEvent == null)
 			OnCrouchEvent = new BoolEvent();
+
+        if (OnClimbEvent == null)
+            OnClimbEvent = new UnityEvent();
+        
+        if (OffGroundEvent == null)
+            OffGroundEvent = new UnityEvent();
 	}
 
 	private void FixedUpdate()
 	{
 		bool wasGrounded = m_Grounded;
-		m_Grounded = false;
-
+		
 		if(IsGrounded())
         {
     		m_Grounded = true;
@@ -55,7 +68,14 @@ public class CharacterController2D : MonoBehaviour
             {
                 OnLandEvent.Invoke();
             }
-		}
+        }else
+        {
+            m_Grounded = false;
+            if(wasGrounded)
+            {
+                OffGroundEvent.Invoke();
+            }
+        }
 	}
 
     private bool IsGrounded()
@@ -73,11 +93,11 @@ public class CharacterController2D : MonoBehaviour
         return false;
     }
 
-    private bool IsClimbing()
+    private bool IsClimbable()
     {
         // The player is climbing if a circlecast to the climb check position hits anything designated as climbable
         // This can be done using layers instead but Sample Assets will not overwrite your project settings
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsClimbable);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_ClimbCheck.position, k_ClimbRadius, m_WhatIsClimbable);
         for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
@@ -88,21 +108,30 @@ public class CharacterController2D : MonoBehaviour
         return false;
     }
 
+    bool IsCrouched(float verticalMove)
+    {
+        bool crouch = verticalMove < 0;
+        // If not crouching, check to see if the character can stand up
+        if (!crouch)
+        {
+            // If the character has a ceiling preventing them from standing up, keep them crouching
+            if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+            {
+                crouch = true;
+            }
+        }
+        return crouch;
+    }
 
-	public void Move(float move, bool crouch, bool jump)
+    public void Move(float horizontalMove, float verticalMove, bool jump)
 	{
-		// If crouching, check to see if the character can stand up
-		if (!crouch)
-		{
-			// If the character has a ceiling preventing them from standing up, keep them crouching
-			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-			{
-				crouch = true;
-			}
-		}
+
+        bool crouch = IsCrouched(verticalMove) && m_Grounded;
+        m_Climbable = IsClimbable();
+        Vector3 targetVelocity;
 
 		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl)
+        if (m_Grounded || m_AirControl || m_Climbable)
 		{
 
 			// If crouching
@@ -115,7 +144,7 @@ public class CharacterController2D : MonoBehaviour
 				}
 
 				// Reduce the speed by the crouchSpeed multiplier
-				move *= m_CrouchSpeed;
+				horizontalMove *= m_CrouchSpeed;
 
 				// Disable one of the colliders when crouching
 				if (m_CrouchDisableCollider != null)
@@ -133,24 +162,39 @@ public class CharacterController2D : MonoBehaviour
 				}
 			}
 
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+            // Move the character by finding the target velocity
+
+
+            if(m_Climbable)
+            {
+                OnClimbEvent.Invoke();
+                m_Rigidbody2D.isKinematic = !m_Grounded;
+                targetVelocity = new Vector2(horizontalMove * 10f, verticalMove * 10f);
+            } else
+            {
+                m_Rigidbody2D.isKinematic = false;
+                targetVelocity = new Vector2(horizontalMove * 10f, m_Rigidbody2D.velocity.y);   
+            }
 			// And then smoothing it out and applying it to the character
 			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
 			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
+			if (horizontalMove > 0 && !m_FacingRight)
 			{
 				// ... flip the player.
 				Flip();
 			}
 			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && m_FacingRight)
+			else if (horizontalMove < 0 && m_FacingRight)
 			{
 				// ... flip the player.
 				Flip();
 			}
 		}
+
+
+        // If the player should climb
+
 		// If the player should jump...
 		if (m_Grounded && jump)
 		{
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,15 +6,21 @@ public class PlayerMovement : MonoBehaviour {
 
     public CharacterController2D controller;
     public float runSpeed = 40f;
+    public float climbSpeed = 20f;
     public Animator animator;
     float horizontalMove = 0f;
+    float verticalMove = 0f;
     bool jump = false;
     bool crouch = false;
+    bool climb = false;
 
 	
 	// Update is called once per frame
 	void Update () {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        verticalMove = Input.GetAxisRaw("Vertical") * climbSpeed;
+        crouch = verticalMove < 0;
+        climb = Mathf.Abs(verticalMove) > 0;
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
@@ -23,28 +29,38 @@ public class PlayerMovement : MonoBehaviour {
             jump = true;
             animator.SetBool("IsJumping", true);
         }
-        if(Input.GetButtonDown("Crouch"))
-        {
-            crouch = true;
-        } else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
-        }
+
+
 	}
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, 
+                        verticalMove * Time.fixedDeltaTime, 
+                        jump);
         jump = false;
 
     }
 
     public void OnLanding(){
+        Debug.Log("Landed");              
         animator.SetBool("IsJumping", false);
+    }
+
+    public void OffGround()
+    {
+        Debug.Log("Off Ground");
+        animator.SetBool("IsJumping", jump);
     }
 
     public void OnCrouch()
     {
         animator.SetBool("IsCrouching", crouch);
+    }
+
+    public void OnClimb()
+    {
+        Debug.Log("Climbing"); 
+        animator.SetBool("IsClimbing", climb);
     }
 }
