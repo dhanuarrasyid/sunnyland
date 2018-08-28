ee76ad309b880427fa2c7b0c66566b706522f3bb
Kyle Barton <kyle.humberto@gmail.com>
2018-08-28 10:16:16 +0200

    added game manager and level exit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject levelChangerPrefab;
+    LevelChanger levelChanger;
+
+	// Use this for initialization
+	void Start () {
+        levelChanger = Instantiate(levelChangerPrefab).GetComponent<LevelChanger>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public  void LevelComplete()
+    {
+        levelChanger.FadeToNextLevel();
+    }
+}
---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LevelExit : MonoBehaviour {
+
+    GameManager gameManager;
+	// Use this for initialization
+	void Start () {
+        gameManager = GetComponentInParent<GameManager>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameManager.LevelComplete();
+    }
+
+}
